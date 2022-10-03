b98d149d5b0c1814298931d445ec307dc8ad1b6f
Jeremiah W. Habets <jeremiah.habets@gmail.com>
2022-10-03 06:44:42 +0000

    tweaking setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,10 @@
 # This is a comment!
 echo Installing Deno 
 curl -fsSL https://deno.land/x/install/install.sh | sh
-export DENO_INSTALL="/home/gitpod/.deno"
-export PATH="$DENO_INSTALL/bin:$PATH"
+echo Updating ENV VARS
+DENO_INSTALL=/home/gitpod/.deno
+PATH=$DENO_INSTALL/bin:$PATH
+echo $DENO_INSTALL
+echo $PATH
+echo Done installing Deno
+touch setupComplete.txt
